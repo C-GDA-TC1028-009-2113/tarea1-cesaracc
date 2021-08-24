50ac663a9ad77967188a63a5228b5b1fa40d761f
César Cabrera <A01642244@itesm.mx>
2021-08-24 00:08:02 +0000

    envío 2

--- assignments/04CalculaTiempo/src/exercise.py
+++ assignments/04CalculaTiempo/src/exercise.py
@@ -1,8 +1,10 @@
 def main():
     #escribe tu código abajo de esta línea
-    pass
-
-
+    import math
+    edad = int(input("Dame tu edad: "))
+    añoa = int(input("Dame el año actual: "))
+    añof = math.ceil((100 - edad) + añoa)
+    print("Cumplirás 100 años en el año: " + str(añof))
 
 
 if __name__ == '__main__':
--- assignments/10Videojuego/src/exercise.py
+++ assignments/10Videojuego/src/exercise.py
@@ -1,7 +1,9 @@
 def main():
     #escribe tu código abajo de esta línea
-    pass
-
+    jnuevos = int(input("Dame la cantidad de juegos nuevos: "))
+    jusados = int(input("Dame la cantidad de juegos usados: "))
+    total = (jnuevos * 1000) + (jusados * 350)
+    print("El total de la compra es: " + str(total))
 
 
 
